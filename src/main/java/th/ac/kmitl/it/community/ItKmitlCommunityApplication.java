103331b2584136f7c80934b6c2871887222729e7
Teerapat Kraisrisirikul <teerapat_saint@hotmail.com>
2024-05-23 09:46:37 +0900

    Initiate Project

---
+++ src/main/java/th/ac/kmitl/it/community/ItKmitlCommunityApplication.java
@@ -0,0 +1,13 @@
+package th.ac.kmitl.it.community;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ItKmitlCommunityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ItKmitlCommunityApplication.class, args);
+    }
+
+}
